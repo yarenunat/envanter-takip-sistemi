6d18a3c8ee98da3cf984daf706dd470e4236fa94
Yaren Unat <111250657+yarenunat@users.noreply.github.com>
2025-08-26 17:11:03 +0300

    Update UpdateStatusView.xaml.cs

--- view/UpdateStatusView.xaml.cs
+++ view/UpdateStatusView.xaml.cs
@@ -6,12 +6,11 @@ namespace WpfApp1.view
 {
     public partial class UpdateStatusView : Window
     {
-        // Bu yapıcı metot, DepotViewModel'dan çağrıldığında çalışır.
-        // DepotItem türünde bir argüman alır ve DataContext'i ayarlar.
         public UpdateStatusView(DepotItem product)
         {
             InitializeComponent();
             DataContext = new UpdateStatusViewModel(product);
         }
     }
+
 }
