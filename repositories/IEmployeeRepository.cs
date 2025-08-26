34885e3ca4924fdde428f1099aa54280fb0cc371
Yaren Unat <111250657+yarenunat@users.noreply.github.com>
2025-08-26 17:08:44 +0300

    Update IEmployeeRepository.cs

--- repositories/IEmployeeRepository.cs
+++ repositories/IEmployeeRepository.cs
@@ -7,15 +7,12 @@ namespace WpfApp1.Repositories
     public interface IEmployeeRepository
     {
         IEnumerable<EmployeeModel> GetEmployees();
-        // Artık sadece categoryName ve departmentName parametreleri alıyor.
         IEnumerable<EmployeeModel> GetEmployeesFiltered(string categoryName, string departmentName);
         IEnumerable<AssignedItemModel> GetAssignedItems(int employeeId);
-
-        // Arayüze GetEmployeeByRegistrationNumber metodu eklendi.
         EmployeeModel GetEmployeeByRegistrationNumber(string registrationNumber);
 
         IEnumerable<string> GetAllCategories();
         IEnumerable<string> GetAllDepartments();
-        // GetAllRoles metodu arayüzden kaldırıldı.
     }
+
 }
